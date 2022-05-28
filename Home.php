5dcea51d57ae5df259022bef97427e08b8663651
youssef mohamed <72819796+youssef-mohamed-aly@users.noreply.github.com>
2022-05-28 16:33:53 +0200

    Update Home.php

--- Home.php
+++ Home.php
@@ -7,16 +7,14 @@ session_start();
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="styles.css">
-    <head>
-        <link rel="stylesheet" href="AboutUsStyle.css">
-        <link rel="stylesheet" href="Styles2.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="AboutUsStyle.css">
+    <link rel="stylesheet" href="Styles2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>About Us</title>
         <link rel="stylesheet" type="text/css" href="Menu.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
     <div class="topnav"> 
       <a  href="home.html"><i class="fa fa-fw fa-home"></i>Home</a>
