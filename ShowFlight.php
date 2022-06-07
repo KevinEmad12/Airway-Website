b45c62965d4a4e3036efe603ddf8cffd66dd9866
MohhammedNasr <96019824+MohhammedNasr@users.noreply.github.com>
2022-06-07 23:38:36 +0200

    Update ShowFlight.php

--- ShowFlight.php
+++ ShowFlight.php
@@ -1,28 +1,31 @@
-    <?php
+<?php
         // Create connection
         $conn = new mysqli("localhost", "root", "", "egway");
         // Check connection
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
         }
-        $sql = "SELECT FlightCode, FromAirPort, Destination, TripStatus FROM trips WHERE trips.FlightCode='".$_GET['FCode']."';";
+        $sql = "SELECT FlightCode, FromAirPort, Destination,f_date,price FROM trips WHERE trips.FlightCode='".$_GET['FCode']."';";
         $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
+        if ($result->num_rows > 0) 
+        {
           // output data of each row
-          while($row = $result->fetch_assoc()) {
+          while($row = $result->fetch_assoc()) 
+          {
             echo"<div class='FlightCardTopBar'></div>";
             echo"<div class='FlightCard'>";
             echo"<div style='text-align: center;'>Flight Details</div>";
-            echo "<span style='text-align: left;'> Flight Code: " . $row["FlightCode"]."</span>";
-            echo "<span style='float:right;'> TripStatus: " . $row["TripStatus"]."</span>";
+            echo "<span style='text-align: left;'> Flight Code: " . $row["FlightCode"]. " </span>";
+            echo "<span style='text-align: left;'> Destination: " . $row["Destination"]. " </span>";
+            echo "<span style='float:right;'> FromAirPort: " . $row["FromAirPort"]. " </span>";
             echo "<br>";
-            echo "<br>";
-            echo "<br>";
-            echo "<span style='text-align: left;'> Destination: " . $row["Destination"]."</span>";
-            echo "<span style='float:right;'> FromAirPort: " . $row["FromAirPort"]."</span>";
+            echo "<span style='float:right;'> price: " . $row["price"]. " </span>";
+            echo "<span style='float:right;'> date: " . $row["f_date"]. " </span>";
             echo "</div>";
           }
-        } else {
+        } 
+        else 
+        {
           echo "<div style='font-size:40px;color:Red;'><br>Flight Code not Found</div>";
         }
         $conn->close();
