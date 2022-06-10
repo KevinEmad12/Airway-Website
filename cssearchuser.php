8755e3f3454b044a67a06c05387bc92fd82ffbcc
mohammedhegazyy <102255891+mohammedhegazyy@users.noreply.github.com>
2022-06-10 22:46:15 +0200

    Update cssearchuser.php

--- cssearchuser.php
+++ cssearchuser.php
@@ -1,10 +1,3 @@
-<style>
-table, th, td {
-  border: 1px solid black;
-}
-
-</style>
-
 <?php
     include "csmenu.php";
     $servername = "localhost";
@@ -23,26 +16,27 @@ table, th, td {
 
     if(isset($_POST['save']))
     {
-        
-        $sql="SELECT * from users WHERE Email='".$_POST['email']."'";
-        $result = mysqli_query($conn,$sql);
-    
-        echo"<table >
+        $email = $_POST['email'];
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        {
+            $sql="SELECT * from users WHERE Email='".$_POST['email']."'";
+            $result = mysqli_query($conn,$sql);
+
+            echo"<table>
     
         <tr>
         
-            <th> Full Name</th>
+            <th>Full Name</th>
             <th>Email</th>
             <th>Password</th>
             <th>National ID</th>
-            <th> Phone Number</th>
-            <th>Address ID</th>
-            <th>Postal Code ID</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            <th>Postal Code </th>
             <th>Country</th>
             <th>Passport Number</th>
             <th>Nationality</th>
-          
-             
+            <th>Date</th>
         </tr>";
     while($row = mysqli_fetch_array($result)) 
     {
@@ -53,18 +47,36 @@ table, th, td {
         <td><?= $row['Email']; ?></td>
         <td><?=  $row['Password']; ?></td>
         <td><?= $row['natid']; ?></td>
-
+        <td><?= $row['phone_num']; ?></td>
+        <td><?= $row['address']; ?></td>
+        <td><?= $row['postal_code']; ?></td>
+        <td><?= $row['country']; ?></td>
+        <td><?= $row['passport_num']; ?></td>
+        <td><?= $row['nationality']; ?></td>
+        <td><?= $row['date_time']; ?></td>
         </tr>
         <?php
     }
     
+}
+    else {
+        $message= "$email. is not a valid email address";
     }
+}
 ?>
-
+        
 <html>
-<form method="post" action="">
-Email: <input type="text" name="email">
- 
-<p><button type="submit" name="save">Search</button></p>
-
+<head>
+    <title>Search For A User</title>
+    <link rel="stylesheet" href="customerservicestyle.css">
+</head>
+<body>
+    <div class ="title"> Search For A User </div>    
+    <form method="post" action="">
+        <div class="search"> Enter Email: </div>
+        <input type="text" class="searchtext" placeholder="example@yahoo.com" name="email" required>
+        <p><button type="submit" name="save" class="searchbutton">Search</button></p>
+    </form>
+    <div class="error"><?php if(isset($message)) { echo $message; } ?></div>
+</body>
 </html>
