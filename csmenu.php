ea8b4af4af9f545ac045d97340abf8a2d9b8bb77
mohammedhegazyy <102255891+mohammedhegazyy@users.noreply.github.com>
2022-06-10 22:44:45 +0200

    Update csmenu.php

--- csmenu.php
+++ csmenu.php
@@ -1,38 +1,60 @@
 <html>
 	<head>
 		<style>
-			.topnav{
-				background-color: #7f1b4e;
-				overflow:hidden;
-				color: white;
-				font-size: 17px;
-				padding: 14px 16px;
-			}
-			.topnav a{
-				float:left;
-				display:block;
-				color: white;
-				text-align: center;
-				padding: 0px 16px;
-				text-decoration: none;
-				font-size: 17px;
-			}
-			.topnav a:hover{
-				background-color: #000000;
-				
-			}
+		.topnav
+		{
+			background-color: #5c0931;
+			width: 100%;
+			display: fixed;
+			top: 0%;
+			left: 0%;
+			overflow:hidden;
+
+		}
+		.topnav a
+		{
+			float: left;
+			display: block;
+			color: #f2f2f2;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration:none;
+			font-size:17px; 
+			cursor: pointer;          
+ 		}
+
+		.topnav-right a
+		{
+		    float: right;
+		    display: block;
+		    color: #f2f2f2;
+		    text-align: center;
+		    padding: 14px 16px;
+		    text-decoration:none;
+		    font-size:17px;
+		}
+         	.topnav a:hover{
+			background-color:whitesmoke ;
+			color: black;
+		}
+			.topnav a:active
+		{
+			background-color: #500944;
+			color: white;
+		}
 		</style>
+
 	</head>
 	<body>		
 		<div class="topnav">
 			<?php
-					echo"<a href='cshome.php'>Home</a>";
-					echo"<a href='csadd.php'>Add Reservation</a>";
-					echo"<a href='csdelete.php'>Delete Reservation</a>";
-                    echo"<a href='csupdate.php'>Edit Reservation</a>";
-                    echo"<a href='cssearchuser.php'>Search for User</a>";
-                    echo"<a href='cssearchflight.php'>Search for Flight</a>";
-				?>
+				echo"<a href='cshome.php'>Home</a>";
+				echo"<a href='csadd.php'>Add Reservation</a>";
+				echo"<a href='csdelete.php'>Delete Reservation</a>";
+                    		echo"<a href='csupdate.php'>Edit Reservation</a>";
+                    		echo"<a href='cssearchuser.php'>Search for User</a>";
+                    		echo"<a href='cssearchflight.php'>Search for Flight</a>";
+			?>
 		</div>
 		<br><br>
 	</body>
