ba0a693148a4aac985832b36fe73cf779d1a724e
mohammedhegazyy <102255891+mohammedhegazyy@users.noreply.github.com>
2022-06-10 22:48:03 +0200

    Update csupdateaction.php

--- csupdateaction.php
+++ csupdateaction.php
@@ -1,57 +1,44 @@
 <?php
-include "csmenu.php";
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "egway";
-session_start();
+ include "csmenu.php";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if(! $conn ) {
-    die('Could not connect: ' . mysql_error());
-}
-    
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "egway";
+ session_start();
+ 
+ $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if(! $conn) 
+    {
+        die('Could not connect: ' . mysql_error());
+    }
 
-if(isset($_POST['submit'])) 
-{
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $revnum = $_POST['rev_num'];
-    $f_code = $_POST['f_code'];
-    $f_code2= $_POST['f_code2'];
-    $f_code3 = $_POST['f_code3'];
-    $date = $_POST['date'];
-
-
-
-    $sql = "UPDATE reservations set name = '$name', rev_num = '$revnum', f_code = '$f_code', f_code2='$f_code2', f_code3 = $f_code3, date = '$date' where id = $id";
-   
-    $result = mysqli_query($conn,$sql);     
-    $message = "Record Modified Successfully";
-
-}
-
-
-$query = "SELECT * FROM reservations where id='" . $_GET['id'] . "'";
-$result = mysqli_query($conn,$query);
-$row= mysqli_fetch_array($result);
-
-
+    if(isset($_POST['submit'])) 
+    {
+        $sql="UPDATE reservations set name='" . $_POST['name'] . "',rev_num='" . $_POST['rev_num'] . "',f_code='" . $_POST['f_code']. "',f_code2='" . $_POST['f_code2']. "',f_code3='" . $_POST['f_code3']. "',date='" . $_POST['date']  
+         . "' WHERE rev_num='" .  $_GET['rev_num'] . "'";
+     
+        mysqli_query($conn,$sql);
+        $message = "Record Modified Successfully";
+    }
+    $result = mysqli_query($conn,"SELECT * FROM reservations WHERE rev_num='" . $_GET['rev_num'] . "'");
+    $row= mysqli_fetch_array($result);
 ?>
-
 
 <html>
 <head>
-<title>Update Reservation</title>
+    <title>Edit Reservation</title>
+    <link rel="stylesheet" href="customerservicestyle.css">
 </head>
 <body>
+    <div class="title">Edit Reservation:</div>
+    <div class = "FlightCardTopBar"> </div> 
+    <div class ="FlightCard">
 <form method="post" action="">
 <div>
     <?php if(isset($message)) { echo $message; } ?>
 </div>
-
-<input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-<br>
+<div style="padding-bottom:5px;"></div>
 Name: <br>
 <input type="text" name="name" value="<?php echo $row['name']; ?>">
 <br>
@@ -68,10 +55,10 @@ Flight Code3: <br>
 <input type="text" name="f_code3"  value="<?php echo $row['f_code3']; ?>">
 <br>
 Date: <br>
-<input type="text" name="date"  value="<?php echo $row['date']; ?>">
+<input type="text" name="date" class="date" value="<?php echo $row['date']; ?>">
 <br>
-<input type="submit" name="submit" value="Update">
+<input type="submit" name="submit" class="button" value="Update">
 </form>
-
+</div>
 </body>
 </html>
