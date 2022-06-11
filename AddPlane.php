6282077aa8ac8adabff458830725c0afa97d066e
Samukon27 <101165517+Samukon27@users.noreply.github.com>
2022-06-11 20:56:04 +0200

    Delete AddPlane.php

--- AddPlane.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$connection=new mysqli('localhost','root','','EGWay');
-	if($connection->connect_error)
-		die("not found");
-	else 
-	    $query="INSERT INTO `airplane`(`MSN`, `AirCraftType`, `NumEconomySeats`, `NumBusinessSeats`) VALUES ('".$_POST['MSN']."','".$_POST['AirCraftType']."','".$_POST['NumEconomySeats']."','".$_POST['NumBusinessSeats']."')";
-	    $result=$connection->query($query);
-?>
