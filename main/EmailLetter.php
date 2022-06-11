30c14a8ea0bce350c09d18a3ec55bd881d4adabf
KevinEmad12 <101450429+KevinEmad12@users.noreply.github.com>
2022-06-11 21:15:34 +0200

    Update EmailLetter.php

--- main/EmailLetter.php
+++ main/EmailLetter.php
@@ -2,9 +2,6 @@
 
 include ("databaseconfig.php");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
