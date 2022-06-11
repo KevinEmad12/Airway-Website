108a4047e232965d56c6cbb292a6de283a11c633
MohhammedNasr <96019824+MohhammedNasr@users.noreply.github.com>
2022-06-11 23:09:39 +0200

    Update and rename header.html to header.php

--- layout/header.php
+++ layout/header.php
@@ -5,7 +5,9 @@ session_start();
 
 
 </html>
-<head> <link rel="stylesheet" href="/apps/xampp/htdocs/project/main/mainstyle.css"></head>
+<head> <link rel="stylesheet" href="/apps/xampp/htdocs/project/main/mainstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
 
 <a href="/project/main/Home.php"><div class="topnav"> 
     <div >
@@ -13,7 +15,7 @@ session_start();
 </div></a>
   <a  href="/project/main/Home.php"><i class="fa fa-fw fa-home"></i>Home</a>
   <a  href="/project/main/ContactUs.php"><i class="fa fa-fw fa-envelope"></i>Contact</a>
-  <a  href="Book.html"><i class="fa fa-plane" aria-hidden="true"></i> Book</a>
+  <a  href="/project/main/AboutUs.php"><i class="fa fa-info-circle" aria-hidden="true"></i> About</a>
   <a href="FlightSearchAid.php"><i class="fa fa-calendar" aria-hidden="true"></i> My Trip</a>
   <div class="topnav-right">
 
