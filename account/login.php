6e74a9a5b9dff5190015131d4befd9051f5ddede
MohhammedNasr <96019824+MohhammedNasr@users.noreply.github.com>
2022-06-12 05:19:18 +0200

    Update login.php

--- account/login.php
+++ account/login.php
@@ -18,10 +18,9 @@ if(isset($_POST['email']) || isset($_POST['password']))
    $dehashpass = md5($password);
    if(!empty($email) && !empty($password))
    {
-   $select = "SELECT * FROM username WHERE email = '$email' AND password = '$dehashpass' ";
+   $select = "SELECT * FROM users WHERE Email = '$email' AND Password = '$dehashpass' ";
    $result = mysqli_query($conn, $select);
    $check = mysqli_num_rows($result);
-
    if($check == 0)
    {
       $response2['status'] = 0;
@@ -30,9 +29,32 @@ if(isset($_POST['email']) || isset($_POST['password']))
    }
    else
    {
-        $response2['status'] = 1;
-        $_SESSION['email'] = $email;
-        $response2['message'] = "<script>window.location = '/project/main/Home.php' </script>";
+
+    $role = "SELECT role FROM `users` WHERE role  = 'qc' && email = '$email' ";
+    $result1 = mysqli_query($conn, $role);
+    $check1 = mysqli_fetch_array($result1);
+
+    $role2 = "SELECT role FROM `users` WHERE role  = 'cs' && email = '$email' ";
+    $result2 = mysqli_query($conn, $role2);
+    $check2 = mysqli_fetch_array($result2);
+
+    if($check1["role"] == "qc")
+    {
+      $response2['status'] = 1;
+      $response2['message'] = "<script>window.location = '/project/qc_home.html' </script>";
+    }
+    else if($check2['role'] == "cs")
+    {
+      $response2['status'] = 1;
+      $response2['message'] = "<script>window.location = '/project/main/AboutUs.php' </script>";
+    }
+    else
+    {
+      $response2['status'] = 1;
+      $_SESSION['User'] = $email;
+      $response2['message'] = "<script>window.location = '/project/main/Home.php' </script>";
+    }
+       
         
    }
    
