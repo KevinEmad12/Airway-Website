5f3c322a730a2771ea5c26c5d5992e1f29578a12
MohhammedNasr <96019824+MohhammedNasr@users.noreply.github.com>
2022-06-13 04:58:43 +0200

    Delete FlightSearchAid.php

--- FlightSearchAid.php
+++
@@ -1,9 +0,0 @@
-
-  <?php
-    $Cities=array("cairo","paris","dubai","london","rome","istanbul","delhi","tokyo","moscow","madrid","Dublin","Luxor");
-    for ($i=0; $i<sizeof($Cities); $i++)
-    {
-        if(strtolower(substr($Cities[$i],0,strlen($_GET['City'])))==strtolower($_GET['City']))
-            echo("<option value='$Cities[$i]'>");
-    }
-    ?>
